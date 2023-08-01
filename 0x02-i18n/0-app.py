dd6d4bde46f0b395d227bcaffac2bc2783da6c5b
Irvine Sunday <amugumbiirvine@gmail.com>
2023-08-01 13:06:56 +0300

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python3
+'''
+    Module for Babel i18n.
+'''
+
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def helloWorld() -> str:
+    '''
+        Render template for Babel usage.
+    '''
+    return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
-
